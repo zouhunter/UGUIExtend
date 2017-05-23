17c0481b3a063c052fa5889b5ce9bba6ef85f649
zouhangte <zouhangtezbm@126.com>
2017-05-23 17:28:06 +0800

    add some scripts

---
+++ Assets/Scripts/UICircle/Editor/UICircleInspector.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEditor.UI;
+
+[CustomEditor(typeof(UICircle), true)]
+[CanEditMultipleObjects]
+public class UICircleInspector : RawImageEditor
+{
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        UICircle circle = target as UICircle;
+        circle.segments = Mathf.Clamp(EditorGUILayout.IntField("UICircle多边形", circle.segments), 4, 360);
+
+    }
+
+}
+
